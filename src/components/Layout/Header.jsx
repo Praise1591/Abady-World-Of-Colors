7fd3c382f69eaa8c7f2b6d846a4628f49da70f6c
Onwubiko Praise <praiseonwubiko@gmail.com>
2025-09-27 15:45:13 +0100

    Setting th layouts

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -3,6 +3,9 @@ import { Search } from 'lucide-react';
 import { Filter } from 'lucide-react';
 import { Plus } from 'lucide-react';
 import { Sun } from 'lucide-react';
+import { Bell } from 'lucide-react'
+import { Settings } from 'lucide-react'
+import { ChevronDown } from 'lucide-react'
 import React from "react";
 
 function Header(){
@@ -43,6 +46,26 @@ function Header(){
                         <Sun className='w-5 h-5'/>
                     </button>
 
+                    {/* Notification */}
+                    <button className='relative p-2.5 rounded-xl text-slate-600 dark:text-slate-300 hover:bg-slate-800 transition-colors'>
+                        <Bell className='w-5 h-5'/>
+                        <span className='absolute -top-1 w-5 h-5 bg-red-500 text-white text-xs rounded-full flex items-center justify-center'>3</span>
+                    </button>
+
+                    {/* Setting */}
+                    <button className='relative p-2.5 rounded-xl text-slate-600 dark:text-slate-300 hover:bg-slate-800 transition-colors'>
+                        <Settings className='w-5 h-5' />
+                    </button>
+
+                    {/* User Profile */}
+                    <div className='flex items-center space-x-3 pl-3 border-l border-b-slate-200 dark:border-slate-700'>
+                        <img src="" alt="User" className='w-8 h-8 rounded-full ring-2 ring-blue-500' />
+                        <div className='hidden md:block'>
+                            <p className='text-sm font-medium text-slate-500 dark:text-slate-400'>Alex Johnson</p>
+                            <p className='text-xs text-slate-500 dark:text-slate-400'>Adminstrator</p>
+                        </div>
+                        <ChevronDown className='w-4 h-4 text-slate-400'/>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -1,8 +1,90 @@
-import { Zap } from "lucide-react";
+import {
+    BarChart3,
+    Calendar,
+    ChevronDown,
+    CreditCard,
+    FileText,
+    LayoutDashboard,
+    MessageSquare,
+    Package,
+    Settings,
+    ShoppingBag,
+    User,
+    Users,
+    Zap,
+} from 'lucide-react';
 import React from "react";
 
-function Sidebar(){
-    return (<div className="transition duration-300 ease-in-out bg-white/80 dark:bg-slate-900/80 backdrop-blur-xl border-r border-slate-700/50 flex flex-col relative z-10">
+const menuItems = [
+    {
+        id: "dashboard",
+        icon: LayoutDashboard,
+        label: "Dashboard",
+        active: true,
+        badge: "New",
+    },
+    {
+        id: "analytics",
+        icon: BarChart3,
+        label: "Analytics",
+        submenu: [
+            {id: "overview", label: "Overview"},
+            {id: "reports", label: "Reports"},
+            {id: "insighs", label: "Insights"},
+        ],
+    },
+    {
+        id: "users",
+        icon: Users,
+        label: "Users",
+        count: "2.4k",
+        submenu: [
+            {id: "all-users", label: "All Users"},
+            {id: "roles", label: "Roles & Permissions"},
+            {id: "activity", label: "User Activity"},
+        ],
+    },
+    {
+        id: "ecommerce",
+        icon: ShoppingBag,
+        label: "E-commerce",
+        submenu: [
+            {id: "products", label: "Products"},
+            {id: "order", label: "Orders"},
+            {id: "customers", label: "Customers"},
+        ],
+    },
+    {
+        id: "inventory",
+        icon: Package,
+        label: "Inventory",
+        count: "847",
+    },
+    {
+        id: "transactions",
+        icon: CreditCard,
+        label: "Transactions",
+    },
+    {
+         id: "mesaages",
+        icon: MessageSquare,
+        label: "Messages",
+        badge: "12",
+    },
+    {
+        id: "reports",
+        icon: FileText,
+        label: "Reports",
+    },
+    {
+        id: "settings",
+        icon: Settings,
+        label: "Settings",
+    },
+]
+
+function Sidebar({ collapsed, onToggle, currentPage, onPageChange }){
+    return (<div className={`${collapsed ? 'w-20' : 'w-72'} transition duration-300 ease-in-out bg-white/80 dark:bg-slate-900/80 backdrop-blur-xl border-r border-slate-700/50 flex flex-col relative z-10`}>
         <div className="p-6 border-b border-slate-200/50 dark:border-slate-700/50">
             <div className="flex items-center space-x-3">
                 <div className="w-10 h-10 bg-gradient-to-r from-blue-600 to-purple-600 rounded-xl flex items-center justify-center shadow-lg">
@@ -18,7 +100,38 @@ function Sidebar(){
         </div>
 
         {/* Navigation */}
-        <nav className="flex-1 p-4 space-y-2 overflow-y-auto"></nav>
+        <nav className="flex-1 p-4 space-y-2 overflow-y-auto">
+            {menuItems.map((item)=>{
+                return(
+                    <div key={item.id}>
+                        <button className={'w-full flex items-center justify-between p-3 rounded-xl transition-all duration-200'}>
+                            <div className='flex items-center space-x-3'>
+                                <item.icon className={'w-5 h-5'} />
+                                {/* Conditional Rendering */}
+                                <>
+                                    <span className='font-medium ml-2'>{item.label}</span>
+                                    {item.badge && (
+                                        <span className='px-2 py-1 text-xs  bg-red-500 text-white- rounded-full'>
+                                            {item.badge}
+                                         </span>
+                                )}
+                                {item.count && <span className='px-2 py-1 text-xs bg-slate-200 dark:bg-slate-700 text-slate-600 dark:text-slate-300 rounded-full'>{item.count}</span>}
+                                </>
+                            </div>
+
+                            {item.submenu && <ChevronDown className='w-4 h-4 transition-transform'/>}
+                        </button>
+
+                        {/* Sub menus */}
+                        <div className='ml-0 mt-2 space-y-1'>
+                            {/*item.submenu.map((subitem) =>{
+                                return <button>{subitem.label}</button>;
+                            })*/}
+                        </div>
+                    </div>
+                );
+            })}
+        </nav>
 
         {/* User Profile */}
         <div className="p-4 border-t border-slate-200/50 dark:border-slate-700/50">

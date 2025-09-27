03b6160f43efaf0bf0e5f9b9210960e3ae957cfe
Onwubiko Praise <praiseonwubiko@gmail.com>
2025-09-27 06:34:37 +0100

    Adding new codes

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,4 +1,8 @@
 import { Menu } from 'lucide-react';
+import { Search } from 'lucide-react';
+import { Filter } from 'lucide-react';
+import { Plus } from 'lucide-react';
+import { Sun } from 'lucide-react';
 import React from "react";
 
 function Header(){
@@ -12,8 +16,33 @@ function Header(){
                     </button>
 
                     <div className='hidden md:block'>
-                        <h1>Dashboard</h1>
+                        <h1 className='text-2xl font-black text-slate-800 dark:text-white'>Dashboard</h1>
+                        <p>Welcome back, Alex! here's what's happening today</p>
                     </div>
+                </div>
+
+                {/* Center */}
+                <div className='flex-1 max-w-md mx-8'>
+                    <div className='relative'>
+                        <Search className='w-4 h-4 absolute left-3 top-1/2 transform  -translate-y-1/2 text-slate-400'/>
+                        <input type="text" placeholder='Search Anything' className='w-full pl-10 pr-4 py-2.5 bg-slate-100 dark:bg-slate-800 border border-slate-200  dark:border-slate-700 rounded-xl text-slate-800 dark:tex-white placeholder:bg-slate-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all' />
+                        <button className='absolute right-2 top-1/2 transform -translate-y-1/2 p-1.5 text-slate-400 hover:text-slate-600 dark:hover:text-slate-300'>
+                            <Filter />
+                        </button>
+                    </div>
+
+                </div>
+                {/* Right */}
+                <div className='flex items-center space-x-3'>
+                    <button className='hidden lg:flex items-center space-x-2 py-2 px-4 bg-gradient-to-r from-blue-500 to-purple-600 text-white rounded-xl hover:shadow-lg transition-all'>
+                        <Plus className='4-4 h-4'/>
+                        <span className='text-sm font-medium'>New</span>
+                    </button>
+                    {/* Toggle */}
+                    <button className='p-2.5 rounded-xl text-slate-600 dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-800 transition-colors'>
+                        <Sun className='w-5 h-5'/>
+                    </button>
+
                 </div>
             </div>
         </div>

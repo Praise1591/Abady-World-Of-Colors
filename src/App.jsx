ae15610d0328fa441ec17523effc636166aaeba9
Onwubiko Praise <praiseonwubiko@gmail.com>
2025-10-04 10:48:58 +0100

    Done working on the header and sidebar. Now focusing on the Dashboard which requires a Stat Grid i'm working on

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Sidebar from './components/Layout/Sidebar';
 import Header from './components/Layout/Header';
+import Dashboard from './components/Dashboard/Dashboard';
 
 
 function App() {
@@ -17,7 +18,13 @@ function App() {
           onPageChange ={SetCurrentPage}
           />
         <div className='flex-1 flex flex-col overflow-hidden'>
-          <Header /> 
+          <Header sidebarCollapsed={sideBarCollapsed} 
+          onToggleSidebar={()=>setSideBarCollapsed(!sideBarCollapsed)}/>
+          <main className='flex-1 overflow-y-auto bg-transparent'>
+            <div className='p-6 space-y-6'>
+              {currentPage === "dashboard" && <Dashboard />}
+            </div>
+          </main>
         </div>
       </div>
     </div>
---
+++ src/components/Dashboard/StatsGrid.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function StatsGrid(){
+    return(
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
+            <div className="bg-white/80 dark:bg-slate-900/80 backdrop-blur-xl rounded-2xl p-6 border border-r-slate-200/50 dark:border-slate-700/50 hover:shadow-xl hover:shadow-slate-200/20 dark:hover:shadow-slate-900/20 transition-all duration-300 group:">
+                <div className="flex items-start justify-between">
+                    <div className="flx-1"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default StatsGrid;

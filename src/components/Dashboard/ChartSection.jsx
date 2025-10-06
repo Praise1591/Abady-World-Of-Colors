6746316050ae54549429be52c13467236f637c98
Onwubiko Praise <praiseonwubiko@gmail.com>
2025-10-06 07:08:10 +0100

    Commiting changes done after adding new files and making adjustments

---
+++ src/components/Dashboard/ChartSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import RevenueChart from "./RevenueChart";
+import SalesCart from "./SalesCart";
+
+function ChartSection(){
+    return <div className="grid grid-cols-1 xl:grid-cols-3 gap-6">
+        <div className="xl:col-span-2">
+            <RevenueChart />
+        </div>
+        <div className="space-y-6">
+            <SalesCart />
+        </div>
+    </div>
+}
+
+export default ChartSection;
